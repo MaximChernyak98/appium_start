c7c2bdb65e397072123448cb1c3465aac41d5740
Maksim Cherniak utk <cherniak_m@utkonos.ru>
2021-03-17 18:51:34 +0300

    find and click element

--- appium_start.py
+++ appium_start.py
@@ -1,14 +1,26 @@
 from appium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 executor = 'http://127.0.0.1:4723/wd/hub'
 
 desired_capabilities = {
-    platformName: "Android",
-    platformVersion: "8",
-    deviceName: "Android Emulator",
-    app: "/path/to/the/downloaded/ApiDemos-debug.apk",
+    "platformName": "Android",
+    "appPackage": "ru.utkonos.android.utkonoid",
+    "deviceName": "R58NB0NN79X",
+    "appActivity": "ru.utkonos.for_customers.online_store.presentation.activity.splash_screen.SplashScreenActivity"
 }
 
-driver = webdriver.Remote(
-    command_executor=executor,
-    desired_capabilities=desired_capabilities)
+
+driver = webdriver.Remote(command_executor=executor,
+                          desired_capabilities=desired_capabilities)
+
+
+time.sleep(15)
+main_button = driver.find_element(By.ID, "ru.utkonos.android.utkonoid:id/action_main")
+print(f'нашел баттон {main_button}')
+time.sleep(15)
+
+main_button.click()
+print(f'нажал баттон {main_button}')
+time.sleep(15).
